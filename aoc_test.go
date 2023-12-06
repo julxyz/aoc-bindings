eef3dcd5315d75fecb803474d88599eb2fd6cb09
jul <jul@jul.xyz>
2023-12-06 19:23:53 +0100

    submit solution

--- aoc_test.go
+++ aoc_test.go
@@ -3,16 +3,21 @@ package aoclib
 import "testing"
 
 func TestSessionCookie(t *testing.T) {
-	helper := NewAoCHelper()
+	helper := NewAoCHelper(2023, 01)
 	if helper.sessionCookie == "" {
 		t.Fatalf("AoCHelper doesnt have a session cookie.")
 	}
 }
 
 func TestInputWithoutCache(t *testing.T) {
-	helper := NewAoCHelper()
-	input := helper.GetInput(2023, 01, true)
+	helper := NewAoCHelper(2023, 01)
+	input := helper.GetInput(true)
 	if input[0] != "76xkqjzqtwonfour" {
 		t.Fatalf("Retrieved input does not match expected input")
 	}
+}
+
+func TestSubmit(t *testing.T) {
+	helper := NewAoCHelper(2023, 01)
+	helper.Submit(1, 54644)
 }
--- main.go
+++ main.go
@@ -2,15 +2,20 @@ package aoclib
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type AoCHelper struct {
+	Year          int
+	Day           int
+	puzzleStatus  int
 	sessionCookie string
 	httpClient    *http.Client
 	cacheDir      string
@@ -24,7 +29,7 @@ func checkErr(err error) {
 }
 
 // NewAoCHelper returns a new AoCHelper initialized with the session cookie.
-func NewAoCHelper() AoCHelper {
+func NewAoCHelper(year int, day int) AoCHelper {
 	tmpdir := os.Getenv("TMP")
 	if tmpdir == "" {
 		tmpdir = "/tmp"
@@ -36,7 +41,10 @@ func NewAoCHelper() AoCHelper {
 			log.Fatal(err)
 		}
 	}
-	return AoCHelper{getCookie(), &http.Client{}, dir}
+	h := AoCHelper{year, day, 0, getCookie(), &http.Client{}, dir}
+	h.puzzleStatus = h.getPuzzleStatus()
+	h.printPuzzleStatus()
+	return h
 }
 
 // getCookie returns the session cookie if available
@@ -49,12 +57,12 @@ func getCookie() string {
 }
 
 // read stored input from cache
-func (h *AoCHelper) readCached(year int, day int) []string {
-	readFile, err := os.Open(filepath.Join(h.cacheDir, h.getFileName(year, day)))
+func (h *AoCHelper) readCached() []string {
+	readFile, err := os.Open(filepath.Join(h.cacheDir, h.getFileName()))
 	if err != nil {
 		// Oh no dangerous recursion watch out this will totally never get stuck here HAHA
-		h.cacheFromWeb(year, day)
-		return h.readCached(year, day)
+		h.cacheFromWeb()
+		return h.readCached()
 	}
 	fileScanner := bufio.NewScanner(readFile)
 	fileScanner.Split(bufio.ScanLines)
@@ -67,29 +75,76 @@ func (h *AoCHelper) readCached(year int, day int) []string {
 }
 
 // get puzzle input from interwebz and store to cache
-func (h *AoCHelper) cacheFromWeb(year int, day int) {
-	req, err := http.NewRequest("GET", fmt.Sprintf("https://adventofcode.com/%d/day/%d/input", year, day), nil)
+func (h *AoCHelper) cacheFromWeb() {
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://adventofcode.com/%d/day/%d/input", h.Year, h.Day), nil)
 	checkErr(err)
 	req.Header.Add("Cookie", "session="+h.sessionCookie)
 	resp, err := h.httpClient.Do(req)
 	checkErr(err)
 	body, err := ioutil.ReadAll(resp.Body)
 	checkErr(err)
-	err = os.WriteFile(filepath.Join(h.cacheDir, h.getFileName(year, day)), body, os.ModePerm)
+	err = os.WriteFile(filepath.Join(h.cacheDir, h.getFileName()), body, os.ModePerm)
 }
 
 // get filename for a given day
-func (h *AoCHelper) getFileName(year int, day int) string {
-	return fmt.Sprintf("%d%20d.txt", year, day)
+func (h *AoCHelper) getFileName() string {
+	return fmt.Sprintf("%d%20d.txt", h.Year, h.Day)
 }
 
 // GetInput retrieves the puzzle input for a certain day as a []string.
 //
 // Also caches the input, use force=true to force overwrite the cache. Please use caching to avoid unnecessary to the AoC servers
-func (h *AoCHelper) GetInput(year int, day int, force bool) []string {
+func (h *AoCHelper) GetInput(force bool) []string {
 	if force {
-		h.cacheFromWeb(year, day)
-		return h.readCached(year, day)
+		h.cacheFromWeb()
+		return h.readCached()
 	}
-	return h.readCached(year, day)
+	return h.readCached()
+}
+
+func (h *AoCHelper) Submit(part int, solution int) bool {
+	body := []byte(fmt.Sprintf("level=%d&answer=%d", part, solution))
+	req, err := http.NewRequest("POST", fmt.Sprintf("https://adventofcode.com/%d/day/%d/answer", h.Year, h.Day), bytes.NewBuffer(body))
+	checkErr(err)
+	req.Header.Add("Cookie", "session="+h.sessionCookie)
+	resp, err := h.httpClient.Do(req)
+	checkErr(err)
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("HTTP Response code != 200")
+	}
+	body, err = ioutil.ReadAll(resp.Body)
+	checkErr(err)
+	bodyString := string(body[:])
+	if strings.Contains(bodyString, "That's not the right answer") {
+		fmt.Println("That answer wasn't correct :c\nPlease wait 60 seconds before submitting the next solution!")
+		return false
+	}
+	fmt.Println("That was the right answer!")
+	return true
+}
+
+// getPuzzleStatus retrieves which Part of the Puzzle has already been solved
+//
+// 0 = unsolved, 1 = half solved, 2 = finished
+func (h *AoCHelper) getPuzzleStatus() int {
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://adventofcode.com/%d/day/%d", h.Year, h.Day), nil)
+	checkErr(err)
+	req.Header.Add("Cookie", "session="+h.sessionCookie)
+	resp, err := h.httpClient.Do(req)
+	checkErr(err)
+	body, err := ioutil.ReadAll(resp.Body)
+	checkErr(err)
+	bodyString := string(body[:])
+	return strings.Count(bodyString, "Your puzzle answer was")
+}
+
+func (h *AoCHelper) printPuzzleStatus() {
+	switch h.puzzleStatus {
+	case 0:
+		fmt.Println("Puzzle has not been solved yet!")
+	case 1:
+		fmt.Println("Part 1 of the puzzle has been solved. No longer submitting solutions for Part 1!")
+	case 2:
+		fmt.Println("The puzzle has been fully solved. No longer submitting any solutions to the AoC Website!")
+	}
 }
